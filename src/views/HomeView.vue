4b53622ab720ed3dafcd0b2931ab8d2ad8927e0b
hhhsuan41 <hhhsuan41@gmail.com>
2022-04-02 15:18:27 +0800

    修版

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,37 +39,91 @@
           <div class="col-12 d-flex justify-content-center paw">
             <img src="https://imgur.com/v6RZO4g.jpg" alt="" />
           </div>
+          <!-- paw -->
+        <div class="bg-white">
+          <div>
+            <div class="spirit_wrap row">
+              <div class="spirit_img col-md-6">
+                <img class="w-100" src="https://imgur.com/SnpDfOS.jpg" alt="" />
+              </div>
 
-          <div
-            class="col-12 col-md-4 sectionb_left d-flex justify-content-center align-items-center"
-          >
-            <img src="https://imgur.com/DpLwooV.jpg" alt="" />
-            <h4>PetPal在做什麼？</h4>
+              <div class="spirit_text_area col-md-6">
+                <div>
+                  <div class="spirit_text">
+                    <h5 class="text-center">貓狗園的資助物資整合</h5>
+                  </div>
+                  <div class="p-5">
+                    <p>
+                      愛園最常面臨的狀況往往是人力的不足，捐贈物資時而過剩時而潰乏，我們將物資的捐贈整合，並密切的跟愛園做更新，以確保每個愛園都有足夠的物資
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="col-12 col-md-8 d-grid gap-5 spirits">
-            <div class="spirit">
-              <h5 style="margin: 20px">貓狗園的資助物資整合</h5>
+
+          <div>
+            <div class="spirit_wrap row flex-row-reverse">
+              <div class="spirit_img col-md-6">
+                <img class="w-100" src="https://imgur.com/US9HVsB.jpg" alt="" />
+              </div>
+
+              <div class="spirit_text_area col-md-6">
+                <div>
+                  <div class="spirit_text">
+                    <h5 class="text-center">專案人員定期訪視及紀錄</h5>
+                  </div>
+                  <div class="p-5">
+                    <p>
+                      我們跟每一個合作的愛園都有專案人員的紀錄及訪視，除了確認環境的狀況之外，也會依動物入住的數量給予建議，數量如果超過負擔就安排轉往其它愛圓，維護動物們的生活品質
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <p>
-              愛園最常面臨的狀況往往是人力的不足，捐贈物資時而過剩時而潰乏，我們將物資的捐贈整合，並密切的跟愛園做更新，以確保每個愛園都有足夠的物資
-            </p>
-            <div class="spirit">
-              <h5 style="margin: 20px">專案人員定期訪視及紀錄</h5>
+          </div>
+
+          <div>
+            <div class="spirit_wrap row">
+              <div class="spirit_img col-md-6">
+                <img class="w-100" src="https://imgur.com/TMOZ4aO.jpg" alt="" />
+              </div>
+
+              <div class="spirit_text_area col-md-6">
+                <div>
+                  <div class="spirit_text">
+                    <h5 class="text-center">平均物資</h5>
+                  </div>
+                  <div class="p-5">
+                    <p>
+                      將物資的狀況透明化，讓所有捐贈者都能在捐贈時知道哪一個愛園目前還需要幫助，也減少過剩屯放的問題
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <p>
-              我們跟每一個合作的愛園都有專案人員的紀錄及訪視，除了確認環境的狀況之外，也會依動物入住的數量給予建議，數量如果超過負擔就安排轉往其它愛圓，維護動物們的生活品質
-            </p>
-            <div class="spirit">
-              <h5 style="margin: 20px">平均物資</h5>
-            </div>
-            <p>
-              將物資的狀況透明化，讓所有捐贈者都能在捐贈時知道哪一個愛園目前還需要幫助，也減少過剩屯放的問題
-            </p>
+          </div>
           </div>
         </div>
       </div>
       <div class="col-12 d-flex justify-content-center paw">
         <img src="https://imgur.com/v6RZO4g.jpg" alt="" />
+      </div>
+      <div class="action">
+        <div class="action_img"></div>
+        <div class="action_text_wrap">
+          <div class="action_text_area bg-light">
+            <h4 class="text-center mb-3">浪浪的困境</h4>
+            <div class="action_text">
+              <p>零撲殺的政策保障了浪浪的生命</p>
+              <p>隨之而來的是資源不足及糧食不足的狀況</p>
+              <p>我們需要你的幫忙</p>
+              <p>讓中途浪浪安心等家</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="text"></div>
       </div>
       <!-- <Carousel :picsrc="selterIntro"></Carousel> -->
       <div class="promote row row-cols-lg-3 row-cols-md-2 row-cols-sm-1">
@@ -79,7 +133,6 @@
           :key="index"
           :selectitem="item"
         ></CardCarousel>
-        <!-- .slice(0, 3) -->
       </div>
     </div>
   </div>
@@ -213,30 +266,46 @@ export default {
     width: 40%;
   }
 }
-
-.spirits {
-  /* text-align: center; */
+.spirit_wrap > div {
+  padding: 0;
 }
-.spirit {
+.spirit_text_area {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.spirit_text {
   position: relative;
-  background-image: url(https://imgur.com/w842bv1.jpg);
-  background-repeat: no-repeat;
-  background-position: center;
-  /* <img src="https://imgur.com/w842bv1.jpg" alt="" /> */
 }
+
 .spirits p {
   padding: 10px 50px;
 }
 
-.spirit img {
-  position: absolute;
-  top: 0;
-  left: 30%;
-  z-index: -1;
+.action {
+  padding-bottom: 100px;
 }
-.spirit h5 {
-  margin-top: 10px;
-  z-index: 1;
+.action_text_wrap {
+  max-width: 820px;
+  padding: 0 20px;
+  margin: -100px auto 0;
+}
+.action_text_area {
+  position: relative;
+  padding: 40px 20px 0;
+}
+.action_img {
+  background-image: url(https://imgur.com/PtYqk0C.jpg);
+  width: 100%;
+  height: 400px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50%;
+  background-attachment: fixed;
+  opacity: 0.8;
+}
+.action_text {
+  line-height: 1rem;
   text-align: center;
 }
 </style>
