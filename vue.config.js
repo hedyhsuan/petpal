6453ec418b6d27da25a1caf766d4bef23d1cf441
hhhsuan41 <hhhsuan41@gmail.com>
2022-04-19 14:27:18 +0800

    修改publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-    publicPath: '/petpal/'  
+    // publicPath: '/petpal/'  
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/production-sub-path/'
+    : '/'
   }
 //   必須要有/斜線/
